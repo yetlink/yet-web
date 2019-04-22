7320bfdd73b17b7e8949236e1ff87c2818553737
zzk <zzk8344@163.com>
2019-04-22 10:43:56 +0800

    项目搭建

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    devServer: {
+    port:6600,
+      proxy: {
+        '/api': {
+          target: 'https://mufeng.me',
+          changeOrigin: true
+        }
+      }
+    }
+  }
